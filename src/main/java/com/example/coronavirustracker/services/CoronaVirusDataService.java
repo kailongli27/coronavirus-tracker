eaeefdc6cae77c8a96a7900c9518ba275d5d9563
kailongli27 <55965406+kailongli27@users.noreply.github.com>
2020-10-18 21:45:44 -0400

    Update CoronaVirusDataService.java

--- src/main/java/com/example/coronavirustracker/services/CoronaVirusDataService.java
+++ src/main/java/com/example/coronavirustracker/services/CoronaVirusDataService.java
@@ -22,6 +22,11 @@ public class CoronaVirusDataService {
     private static String VIRUS_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
 
     private List<LocationStats> allStats = new ArrayList<>();
+
+    public List<LocationStats> getAllStats() {
+        return allStats;
+    }
+
     @PostConstruct
     @Scheduled(cron = "* * 1 * * *")
     public void fetchVirusData () throws IOException, InterruptedException {
@@ -45,7 +50,7 @@ public class CoronaVirusDataService {
             locationStat.setCountry(record.get("Country/Region"));
             // Get latest total cases from the last column
             locationStat.setLatestTotalCases(Integer.parseInt(record.get(record.size() - 1)));
-            System.out.println(locationStat);
+            // System.out.println(locationStat);
             newStats.add(locationStat);
         }
         this.allStats = newStats;
